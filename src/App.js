c19a5f267c27947107147f035c3687aad979fc95
hayun <hs183000@naver.com>
2022-11-27 00:01:01 +0900

    map() 사용하여 코드 줄이기

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
-import React, {Component} from "react"
-import "./App.css"
-export default class App extends Component{
+import React, {Component} from "react";
+import "./App.css";
+export default class App extends Component {
+  btnStyle = {
+    color: "#fff",
+    padding: "5px 9px",
+    border: "none",
+    borderRadius: "50px",
+    cursor: "pointer",
+    float: "right",
+  };
+  getStyle = () => {
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: "none",
+    };
+  };
+  tododata = [
+    {
+      id: "1",
+      title: "공부하기",
+      completed: true,
+    },
+    {
+      id: "2",
+      title: "청소하기",
+      completed: true,
+    },
+  ];
   render() {
-    return ( 
+    return (
       <div className="container">
         <div className="todoBlock">
           <div className="title">
-             <h1>할 일 목록</h1>
-         </div>
+            <h1>할 일 목록</h1>
+          </div>
+          {this.tododata.map((data) => (
+            <div style={this.getStyle()} key={data.id}>
+              <input type="checkbox" defaultChecked={false} />
+              {data.title}
+              <button style={this.btnStyle}>x</button>
+            </div>
+          ))}
         </div>
       </div>
-
-    )
+    );
   }
 }
