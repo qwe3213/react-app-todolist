a2231817b88b615829ec104f5a3d14e3ef3b1c6e
hayun <hs183000@naver.com>
2022-11-27 19:17:27 +0900

    체크박스 클릭시 선 그어지기

--- src/App.js
+++ src/App.js
@@ -2,18 +2,8 @@ import React, {Component} from "react";
 import "./App.css";
 export default class App extends Component {
   state = {
-    tododata: [
-      {
-        id: "1",
-        title: "공부하기",
-        completed: true,
-      },
-      {
-        id: "2",
-        title: "청소하기",
-        completed: true,
-      },
-    ],
+    tododata: [],
+    value: "",
   };
   btnStyle = {
     color: "#fff",
@@ -23,18 +13,40 @@ export default class App extends Component {
     cursor: "pointer",
     float: "right",
   };
-  getStyle = () => {
+  getStyle = (completed) => {
     return {
       padding: "10px",
       borderBottom: "1px #ccc dotted",
-      textDecoration: "none",
+      textDecoration: completed ? "line-through" : "none",
     };
   };
 
   handleClick = (id) => {
-    let newTodoData = this.state.tododata.filter((data) => data.id !== id);
-    console.log("newTodoData", newTodoData);
+    let newTodoData = this.state.tododata.filter((data) => data.id !== id); // 입력한것 지우기
     this.setState({tododata: newTodoData});
+  };
+  handleChange = (e) => {
+    this.setState({value: e.target.value}); //입렵한 것이 text에 보이게 하기.
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    //새로운 할일 데이터
+    let newTodo = {
+      id: Date.now(),
+      title: this.state.value,
+      completed: false,
+    };
+    //원래 있던 할 일에 새로운 할 일 더해주기
+    this.setState({tododata: [...this.state.tododata, newTodo], value: ""}); // this.state.tododata 원래 있떤 데이터 newTodo 새로운 데이터  배열밖의 값은 입력 엔터후 텍스트창 빈칸으로 만들기
+  };
+  handleCompleteChange = (id) => {
+    let newTodoData = this.state.tododata.map((data) => {
+      if (data.id === id) {
+        data.completed = !data.completed;
+      }
+      return data;
+    });
+    this.setState({tadodata: newTodoData});
   };
   render() {
     return (
@@ -44,8 +56,12 @@ export default class App extends Component {
             <h1>할 일 목록</h1>
           </div>
           {this.state.tododata.map((data) => (
-            <div style={this.getStyle()} key={data.id}>
-              <input type="checkbox" defaultChecked={false} />
+            <div style={this.getStyle(data.completed)} key={data.id}>
+              <input
+                type="checkbox"
+                defaultChecked={false}
+                onChange={() => this.handleCompleteChange(data.id)}
+              />
               {data.title}
               <button
                 style={this.btnStyle}
@@ -55,6 +71,22 @@ export default class App extends Component {
               </button>
             </div>
           ))}
+          <form style={{display: "flex"}} onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              name="value"
+              placeholder="해야 할일을 입력해주세요"
+              style={{flex: "10", padding: "5px"}}
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+            <input
+              type="submit"
+              value="입력"
+              className="btn"
+              style={{flex: "1"}}
+            />
+          </form>
         </div>
       </div>
     );
