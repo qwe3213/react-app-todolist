7d4074c796dd67a715f43995c6741e0f27e7406f
hayun <hs183000@naver.com>
2022-11-30 00:07:58 +0900

    컴포넌트 분리하기 2

--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,6 @@
-import React, {useState} from "react";
+import React from "react";
 
-export default function List() {
+export default function List({tododata, setTododata}) {
   const btnStyle = {
     color: "#fff",
     padding: "5px 9px",
@@ -9,6 +9,28 @@ export default function List() {
     cursor: "pointer",
     float: "right",
   };
+  const handleCompleteChange = (id) => {
+    let newTodoData = tododata.map((data) => {
+      if (data.id === id) {
+        data.completed = !data.completed;
+      }
+      return data;
+    });
+    setTododata(newTodoData);
+  };
+  const getStyle = (completed) => {
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: completed ? "line-through" : "none",
+    };
+  };
+
+  const handleClick = (id) => {
+    let newTodoData = tododata.filter((data) => data.id !== id); // 입력한것 지우기
+    setTododata(newTodoData);
+  };
+
   return (
     <div>
       {tododata.map((data) => (
