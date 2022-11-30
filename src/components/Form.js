e58aa5bc59442d96d17006bf6ab55235d065ce0b
hayun <hs183000@naver.com>
2022-11-30 18:53:56 +0900

    Form부분 Tailwind css 적용

--- src/components/Form.js
+++ src/components/Form.js
@@ -17,16 +17,20 @@ export default function Form({value, setValue, setTododata}) {
   };
   return (
     <div>
-      <form style={{display: "flex"}} onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="flex pt-2">
         <input
           type="text"
           name="value"
+          className="w-full px-3 py-2 text-gray-500 border rounded shadow"
           placeholder="해야 할일을 입력해주세요"
-          style={{flex: "10", padding: "5px"}}
           value={value}
           onChange={handleChange}
         />
-        <input type="submit" value="입력" className="btn" style={{flex: "1"}} />
+        <input
+          type="submit"
+          value="입력"
+          className=" p-2 text-blue-400 border-2  border-blue-400 rounded hover:text-white hover:bg-blue-200 "
+        />
       </form>
     </div>
   );
